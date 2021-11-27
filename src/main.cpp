546bb03288923f5cc92319d303d35031bfa55194
Tom Mullier <tom.mullier@outlook.fr>
2021-11-27 17:51:02 +0100

    add smfl test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <SFML/Graphics.hpp>
 #include <cmath>
 #include <fstream>
 #include <iostream>
@@ -8,6 +9,7 @@
 #include "../headers/plane.hpp"
 
 using namespace std;
+using namespace sf;
 
 int main(int argc, char *argv[]) {
   srand(time(NULL));
@@ -15,7 +17,24 @@ int main(int argc, char *argv[]) {
   cout << France;
   cout << "-------------------" << endl << endl;
 
+  // RenderWindow window(VideoMode(200, 200), "SFML works!");
+  // CircleShape shape(100.f);
+  // shape.setFillColor(Color::Green);
+
+  // while (window.isOpen()) {
+  //   Event event;
+  //   while (window.pollEvent(event)) {
+  //     if (event.type == Event::Closed)
+  //       window.close();
+  //   }
+
+  //   window.clear();
+  //   window.draw(shape);
+  //   window.display();
+  // }
+
   Plane p(France);
   p.setParameters(France);
   cout << p << endl;
+  // p.navigate();
 }
